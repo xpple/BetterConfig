d4b029b953fa0e1c063aac60e26c9471d2244b70
Joe <burtonjae@hotmail.co.uk>
2023-05-31 22:29:10 +0100

    Update to 1.20

--- src/main/java/dev/xpple/betterconfig/command/ConfigCommand.java
+++ src/main/java/dev/xpple/betterconfig/command/ConfigCommand.java
@@ -14,42 +14,42 @@ public class ConfigCommand extends ConfigCommandHelper<ServerCommandSource> {
 
     @Override
     protected int get(ServerCommandSource source, ModConfigImpl modConfig, String config) {
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.get", config, modConfig.asString(config)), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.get", config, modConfig.asString(config)), true);
         return Command.SINGLE_SUCCESS;
     }
 
     @Override
     protected int reset(ServerCommandSource source, ModConfigImpl modConfig, String config) {
         modConfig.reset(config);
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.reset", config, modConfig.asString(config)), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.reset", config, modConfig.asString(config)), true);
         return Command.SINGLE_SUCCESS;
     }
 
     @Override
     protected int set(ServerCommandSource source, ModConfigImpl modConfig, String config, Object value) throws CommandSyntaxException {
         modConfig.set(config, value);
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.set", config, modConfig.asString(config)), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.set", config, modConfig.asString(config)), true);
         return Command.SINGLE_SUCCESS;
     }
 
     @Override
     protected int add(ServerCommandSource source, ModConfigImpl modConfig, String config, Object value) throws CommandSyntaxException {
         modConfig.add(config, value);
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.add", modConfig.asString(value), config), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.add", modConfig.asString(value), config), true);
         return Command.SINGLE_SUCCESS;
     }
 
     @Override
     protected int put(ServerCommandSource source, ModConfigImpl modConfig, String config, Object key, Object value) throws CommandSyntaxException {
         modConfig.put(config, key, value);
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.put", key, modConfig.asString(value), config), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.put", key, modConfig.asString(value), config), true);
         return Command.SINGLE_SUCCESS;
     }
 
     @Override
     protected int remove(ServerCommandSource source, ModConfigImpl modConfig, String config, Object value) throws CommandSyntaxException {
         modConfig.remove(config, value);
-        source.sendFeedback(Text.translatable("betterconfig.commands.config.remove", modConfig.asString(value), config), true);
+        source.sendFeedback(() -> Text.translatable("betterconfig.commands.config.remove", modConfig.asString(value), config), true);
         return Command.SINGLE_SUCCESS;
     }
 }
