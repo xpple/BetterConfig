2f5fb0a3f8edb3046c8b5fbd23a7d333e15d023a
xpple <fred@xpple.dev>
2024-04-22 15:46:23 +0200

    Clean up command building

--- common/src/main/java/dev/xpple/betterconfig/command/AbstractConfigCommand.java
+++ common/src/main/java/dev/xpple/betterconfig/command/AbstractConfigCommand.java
@@ -13,8 +13,6 @@ import dev.xpple.betterconfig.util.CheckedFunction;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Predicate;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.*;
@@ -33,112 +31,124 @@ public abstract class AbstractConfigCommand<S, C>  {
     protected final LiteralArgumentBuilder<S> create(Collection<? extends AbstractConfigImpl<S, C>> abstractConfigs, C buildContext) {
         LiteralArgumentBuilder<S> root = LiteralArgumentBuilder.literal(this.rootLiteral);
         for (AbstractConfigImpl<S, C> abstractConfig : abstractConfigs) {
-            Map<String, LiteralArgumentBuilder<S>> literals = new HashMap<>();
+            LiteralArgumentBuilder<S> identifierLiteral = LiteralArgumentBuilder.literal(abstractConfig.getIdentifier());
             for (String config : abstractConfig.getConfigs().keySet()) {
                 Predicate<S> condition = abstractConfig.getConditions().get(config);
                 LiteralArgumentBuilder<S> configLiteral = LiteralArgumentBuilder.<S>literal(config).requires(condition);
-                literals.put(config, configLiteral);
 
                 configLiteral.then(LiteralArgumentBuilder.<S>literal("get").executes(ctx -> get(ctx.getSource(), abstractConfig, config)));
                 configLiteral.then(LiteralArgumentBuilder.<S>literal("reset").executes(ctx -> reset(ctx.getSource(), abstractConfig, config)));
+
+                String comment = abstractConfig.getComments().get(config);
+                if (comment != null) {
+                    configLiteral.then(LiteralArgumentBuilder.<S>literal("comment").executes(ctx -> comment(ctx.getSource(), config, comment)));
+                }
+
+                if (abstractConfig.getSetters().containsKey(config)) {
+                    Config annotation = abstractConfig.getAnnotations().get(config);
+                    Config.Setter setter = annotation.setter();
+                    Class<?> type = setter.type() == Config.EMPTY.class ? abstractConfig.getType(config) : setter.type();
+                    var argumentFunction = abstractConfig.getArgument(type);
+                    if (argumentFunction != null) {
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
+                        subCommand.executes(ctx -> set(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("set").then(subCommand));
+                    } else if (type.isEnum()) {
+                        //noinspection rawtypes, unchecked
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
+                        subCommand.executes(ctx -> {
+                            String value = getString(ctx, "value");
+                            return set(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
+                        });
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("set").then(subCommand));
+                    }
+                }
+
+                if (abstractConfig.getAdders().containsKey(config)) {
+                    Config annotation = abstractConfig.getAnnotations().get(config);
+                    Config.Adder adder = annotation.adder();
+                    Class<?> type = adder.type() == Config.EMPTY.class ? (Class<?>) abstractConfig.getParameterTypes(config)[0] : adder.type();
+                    var argumentFunction = abstractConfig.getArgument(type);
+                    if (argumentFunction != null) {
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
+                        subCommand.executes(ctx -> add(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("add").then(subCommand));
+                    } else if (type.isEnum()) {
+                        // noinspection rawtypes, unchecked
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
+                        subCommand.executes(ctx -> {
+                            String value = getString(ctx, "value");
+                            return add(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
+                        });
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("add").then(subCommand));
+                    }
+                }
+
+                if (abstractConfig.getPutters().containsKey(config)) {
+                    Config annotation = abstractConfig.getAnnotations().get(config);
+                    Config.Putter putter = annotation.putter();
+                    Type[] types = abstractConfig.getParameterTypes(config);
+                    Class<?> keyType = putter.keyType() == Config.EMPTY.class ? (Class<?>) types[0] : putter.keyType();
+                    RequiredArgumentBuilder<S, ?> subCommand;
+                    CheckedFunction<CommandContext<S>, ?, CommandSyntaxException> getKey;
+                    var keyArgumentFunction = abstractConfig.getArgument(keyType);
+                    if (keyArgumentFunction != null) {
+                        subCommand = RequiredArgumentBuilder.argument("key", keyArgumentFunction.apply(buildContext));
+                        getKey = ctx -> ctx.getArgument("key", keyType);
+                    } else if (keyType.isEnum()) {
+                        //noinspection rawtypes, unchecked
+                        subCommand = RequiredArgumentBuilder.argument("key", string()).suggests(this.enumSuggestionProvider((Class) keyType));
+                        getKey = ctx -> {
+                            String value = getString(ctx, "key");
+                            return Arrays.stream(keyType.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value));
+                        };
+                    } else {
+                        subCommand = null;
+                        getKey = null;
+                    }
+                    if (subCommand != null) {
+                        Class<?> valueType = putter.valueType() == Config.EMPTY.class ? (Class<?>) types[1] : putter.valueType();
+                        var valueArgumentFunction = abstractConfig.getArgument(valueType);
+                        if (valueArgumentFunction != null) {
+                            RequiredArgumentBuilder<S, ?> subSubCommand = RequiredArgumentBuilder.argument("value", valueArgumentFunction.apply(buildContext));
+                            subSubCommand.executes(ctx -> put(ctx.getSource(), abstractConfig, config, getKey.apply(ctx), ctx.getArgument("value", valueType)));
+                            configLiteral.then(LiteralArgumentBuilder.<S>literal("put").then(subCommand.then(subSubCommand)));
+                        } else if (valueType.isEnum()) {
+                            //noinspection rawtypes, unchecked
+                            RequiredArgumentBuilder<S, ?> subSubCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) valueType));
+                            subCommand.executes(ctx -> {
+                                String value = getString(ctx, "value");
+                                return put(ctx.getSource(), abstractConfig, config, getKey.apply(ctx), Arrays.stream(valueType.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
+                            });
+                            configLiteral.then(LiteralArgumentBuilder.<S>literal("put").then(subCommand.then(subSubCommand)));
+                        }
+                    }
+                }
+
+                if (abstractConfig.getRemovers().containsKey(config)) {
+                    Config annotation = abstractConfig.getAnnotations().get(config);
+                    Config.Remover remover = annotation.remover();
+                    Class<?> type = remover.type() == Config.EMPTY.class ? (Class<?>) abstractConfig.getParameterTypes(config)[0] : remover.type();
+                    var argumentFunction = abstractConfig.getArgument(type);
+                    if (argumentFunction != null) {
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
+                        subCommand.executes(ctx -> remove(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("remove").then(subCommand));
+                    } else if (type.isEnum()) {
+                        //noinspection rawtypes, unchecked
+                        RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
+                        subCommand.executes(ctx -> {
+                            String value = getString(ctx, "value");
+                            return remove(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
+                        });
+                        configLiteral.then(LiteralArgumentBuilder.<S>literal("remove").then(subCommand));
+                    }
+                }
+
+                identifierLiteral.then(configLiteral);
             }
 
-            abstractConfig.getComments().forEach((config, comment) -> literals.get(config).then(LiteralArgumentBuilder.<S>literal("comment").executes(ctx -> comment(ctx.getSource(), config, comment))));
-            abstractConfig.getSetters().keySet().forEach(config -> {
-                Config annotation = abstractConfig.getAnnotations().get(config);
-                Config.Setter setter = annotation.setter();
-                Class<?> type = setter.type() == Config.EMPTY.class ? abstractConfig.getType(config) : setter.type();
-                var argumentFunction = abstractConfig.getArgument(type);
-                if (argumentFunction != null) {
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
-                    subCommand.executes(ctx -> set(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("set").then(subCommand));
-                } else if (type.isEnum()) {
-                    //noinspection rawtypes, unchecked
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
-                    subCommand.executes(ctx -> {
-                        String value = getString(ctx, "value");
-                        return set(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
-                    });
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("set").then(subCommand));
-                }
-            });
-            abstractConfig.getAdders().keySet().forEach(config -> {
-                Config annotation = abstractConfig.getAnnotations().get(config);
-                Config.Adder adder = annotation.adder();
-                Class<?> type = adder.type() == Config.EMPTY.class ? (Class<?>) abstractConfig.getParameterTypes(config)[0] : adder.type();
-                var argumentFunction = abstractConfig.getArgument(type);
-                if (argumentFunction != null) {
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
-                    subCommand.executes(ctx -> add(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("add").then(subCommand));
-                } else if (type.isEnum()) {
-                    //noinspection rawtypes, unchecked
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
-                    subCommand.executes(ctx -> {
-                        String value = getString(ctx, "value");
-                        return add(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
-                    });
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("add").then(subCommand));
-                }
-            });
-            abstractConfig.getPutters().keySet().forEach(config -> {
-                Config annotation = abstractConfig.getAnnotations().get(config);
-                Config.Putter putter = annotation.putter();
-                Type[] types = abstractConfig.getParameterTypes(config);
-                Class<?> keyType = putter.keyType() == Config.EMPTY.class ? (Class<?>) types[0] : putter.keyType();
-                RequiredArgumentBuilder<S, ?> subCommand;
-                CheckedFunction<CommandContext<S>, ?, CommandSyntaxException> getKey;
-                var keyArgumentFunction = abstractConfig.getArgument(keyType);
-                if (keyArgumentFunction != null) {
-                    subCommand = RequiredArgumentBuilder.argument("key", keyArgumentFunction.apply(buildContext));
-                    getKey = ctx -> ctx.getArgument("key", keyType);
-                } else if (keyType.isEnum()) {
-                    //noinspection rawtypes, unchecked
-                    subCommand = RequiredArgumentBuilder.argument("key", string()).suggests(this.enumSuggestionProvider((Class) keyType));
-                    getKey = ctx -> {
-                        String value = getString(ctx, "key");
-                        return Arrays.stream(keyType.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value));
-                    };
-                } else {
-                    return;
-                }
-                Class<?> valueType = putter.valueType() == Config.EMPTY.class ? (Class<?>) types[1] : putter.valueType();
-                var valueArgumentFunction = abstractConfig.getArgument(valueType);
-                if (valueArgumentFunction != null) {
-                    RequiredArgumentBuilder<S, ?> subSubCommand = RequiredArgumentBuilder.argument("value", valueArgumentFunction.apply(buildContext));
-                    subSubCommand.executes(ctx -> put(ctx.getSource(), abstractConfig, config, getKey.apply(ctx), ctx.getArgument("value", valueType)));
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("put").then(subCommand.then(subSubCommand)));
-                } else if (valueType.isEnum()) {
-                    //noinspection rawtypes, unchecked
-                    RequiredArgumentBuilder<S, ?> subSubCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) valueType));
-                    subCommand.executes(ctx -> {
-                        String value = getString(ctx, "value");
-                        return put(ctx.getSource(), abstractConfig, config, getKey.apply(ctx), Arrays.stream(valueType.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
-                    });
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("put").then(subCommand.then(subSubCommand)));
-                }
-            });
-            abstractConfig.getRemovers().keySet().forEach(config -> {
-                Config annotation = abstractConfig.getAnnotations().get(config);
-                Config.Remover remover = annotation.remover();
-                Class<?> type = remover.type() == Config.EMPTY.class ? (Class<?>) abstractConfig.getParameterTypes(config)[0] : remover.type();
-                var argumentFunction = abstractConfig.getArgument(type);
-                if (argumentFunction != null) {
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", argumentFunction.apply(buildContext));
-                    subCommand.executes(ctx -> remove(ctx.getSource(), abstractConfig, config, ctx.getArgument("value", type)));
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("remove").then(subCommand));
-                } else if (type.isEnum()) {
-                    //noinspection rawtypes, unchecked
-                    RequiredArgumentBuilder<S, ?> subCommand = RequiredArgumentBuilder.argument("value", string()).suggests(this.enumSuggestionProvider((Class) type));
-                    subCommand.executes(ctx -> {
-                        String value = getString(ctx, "value");
-                        return remove(ctx.getSource(), abstractConfig, config, Arrays.stream(type.getEnumConstants()).filter(c -> ((Enum<?>) c).name().equals(value)).findAny().orElseThrow(() -> this.invalidEnumException().create(value)));
-                    });
-                    literals.get(config).then(LiteralArgumentBuilder.<S>literal("remove").then(subCommand));
-                }
-            });
-            literals.values().forEach(literal -> root.then(LiteralArgumentBuilder.<S>literal(abstractConfig.getIdentifier()).then(literal)));
+            root.then(identifierLiteral);
         }
         return root;
     }
