9ca48d95a784fd042d7aac2a26e6253f5135cb54
xpple <fred@xpple.dev>
2025-01-14 21:35:31 +0100

    Actually implement platform-specific API interface

--- paper/src/main/java/dev/xpple/betterconfig/impl/BetterConfigImpl.java
+++ paper/src/main/java/dev/xpple/betterconfig/impl/BetterConfigImpl.java
@@ -1,8 +1,8 @@
 package dev.xpple.betterconfig.impl;
 
-import dev.xpple.betterconfig.api.AbstractBetterConfigAPI;
+import dev.xpple.betterconfig.api.BetterConfigAPI;
 import net.kyori.adventure.text.Component;
 
-public class BetterConfigImpl extends AbstractBetterConfigImpl<Component> implements AbstractBetterConfigAPI<Component> {
+public class BetterConfigImpl extends AbstractBetterConfigImpl<Component> implements BetterConfigAPI {
     public static final AbstractBetterConfigImpl<?> INSTANCE = new BetterConfigImpl();
 }
