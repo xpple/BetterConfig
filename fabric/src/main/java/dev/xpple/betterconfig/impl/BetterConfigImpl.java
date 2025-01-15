12062613d36cb180a5433d9368875b848a2c3c2b
xpple <fred@xpple.dev>
2025-01-15 14:57:51 +0100

    Tighten type bound

--- fabric/src/main/java/dev/xpple/betterconfig/impl/BetterConfigImpl.java
+++ fabric/src/main/java/dev/xpple/betterconfig/impl/BetterConfigImpl.java
@@ -4,5 +4,5 @@ import dev.xpple.betterconfig.api.BetterConfigAPI;
 import net.minecraft.network.chat.Component;
 
 public class BetterConfigImpl extends AbstractBetterConfigImpl<Component> implements BetterConfigAPI {
-    public static final AbstractBetterConfigImpl<?> INSTANCE = new BetterConfigImpl();
+    public static final BetterConfigImpl INSTANCE = new BetterConfigImpl();
 }
